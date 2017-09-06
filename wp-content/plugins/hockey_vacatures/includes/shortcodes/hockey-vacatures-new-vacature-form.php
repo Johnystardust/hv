57d198af8a6ada07d6480fdc6b0cf20b0aaabc14
Tim <timvdslik@gmail.com>
2017-09-06 22:00:37 +0200

    added Hockey_Vacatures_Forms

--- wp-content/plugins/hockey_vacatures/includes/shortcodes/hockey-vacatures-new-vacature-form.php
+++ wp-content/plugins/hockey_vacatures/includes/shortcodes/hockey-vacatures-new-vacature-form.php
@@ -4,7 +4,9 @@
  *
  * @since   1.0.0
  */
-class Hockey_Vacatures_New_Vacature_Form {
+require_once plugin_dir_path( dirname( __FILE__ ) ) . '/hockey-vacatures-forms.php';
+
+class Hockey_Vacatures_New_Vacature_Form extends Hockey_Vacatures_Forms {
 
     private $user_info;
 
@@ -18,7 +20,6 @@ class Hockey_Vacatures_New_Vacature_Form {
     private $content;
 
     public function __construct(){
-
     }
 
     public function new_vacature_form(){
@@ -100,71 +101,14 @@ class Hockey_Vacatures_New_Vacature_Form {
         <?php
     }
 
-    /**
-     * Form builder Function
-     *
-     * @since   1.0.0
-     *
-     * @param array $form_fields
-     *
-     */
-    private function build_form($form_fields = array()){
-        if(!is_array($form_fields)){
-            return;
-        }
-
-        $fields_html = '';
-
-        foreach($form_fields as $field){
-
-            switch($field['type']){
-                case('text'):
-                case('number'):
-                case('password'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>">
-                            <?php esc_attr_e($field['label']); ?>
-                            <?php if(array_key_exists('required', $field) && $field['required']): ?>
-                                <span class="required">*</span>
-                            <?php endif; ?>
-                        </label>
-                        <input id="<?php echo $field['name']; ?>" class="form-control" type="<?php echo $field['type']; ?>" name="<?php echo $field['name']; ?>"
-                               placeholder="<?php echo $field['placeholder']; ?>" value="<?php if($field['name'] !== 'password_check'){ echo(isset($_POST[$field['name']]) ? $_POST[$field['name']] : null); } ?>">
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                    <?php break;
-                case('select'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>"><?php echo $field['label'] ?></label>
-                        <select class="form-control custom-select" name="<?php echo $field['name']; ?>" id="<?php echo $field['name']; ?>">
-                            <?php foreach( $field['options'] as $option => $value ): ?>
-                                <option <?php if(isset($_POST[$field['name']]) && $_POST[$field['name']] == $option){ echo 'selected'; }; ?> value="<?php echo $option; ?>"><?php echo $value; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                    <?php break;
-                case('textarea'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>"><?php echo $field['label'] ?></label>
-                        <textarea class="form-control" name="<?php echo $field['name']; ?>" id="<?php echo $field['name']; ?>" cols="<?php echo $field['cols']; ?>" rows="<?php echo $field['rows']; ?>"
-                                  placeholder="<?php echo $field['placeholder']; ?>"><?php echo(isset($_POST[$field['name']]) ? $_POST[$field['name']] : null); ?></textarea>
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                    <?php break;
-            }
-        }
-
-        echo $fields_html;
-    }
-
     private function add_new_vacature_meta($post_id){
+
+
+
+
+
+
+
         $user_info = get_userdata(get_current_user_id());
         $additional_data = get_user_meta(get_current_user_id(), 'user_data', true);
 
@@ -185,6 +129,22 @@ class Hockey_Vacatures_New_Vacature_Form {
     public function new_vacature_form_shortcode(){
 
         if(isset($_POST['submit'])){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             $this->title    = $_POST['title'];
             $this->function = $_POST['function'];
--- wp-content/plugins/hockey_vacatures/includes/shortcodes/hockey-vacatures-register-form.php
+++ wp-content/plugins/hockey_vacatures/includes/shortcodes/hockey-vacatures-register-form.php
@@ -4,7 +4,9 @@
  *
  * @since   1.0.0
  */
-class Hockey_Vacatures_Register_Form {
+require_once plugin_dir_path( dirname( __FILE__ ) ) . '/hockey-vacatures-forms.php';
+
+class Hockey_Vacatures_Register_Form extends Hockey_Vacatures_Forms {
 
     // General Vars
     private $username;
@@ -325,87 +327,6 @@ class Hockey_Vacatures_Register_Form {
 
         </form>
         <?php
-    }
-
-    /**
-     * Form builder Function
-     *
-     * @since   1.0.0
-     *
-     * @param array $form_fields
-     *
-     * TODO: FIX MULTIPLE FUNCTION
-     *
-     */
-    private function build_form($form_fields = array()){
-        if(!is_array($form_fields)){
-            return;
-        }
-
-        $fields_html = '';
-
-        foreach($form_fields as $field){
-
-            switch($field['type']){
-                case('text'):
-                case('number'):
-                case('password'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>">
-                            <?php esc_attr_e($field['label']); ?>
-                            <?php if(array_key_exists('required', $field) && $field['required']): ?>
-                                <span class="required">*</span>
-                            <?php endif; ?>
-                        </label>
-                        <input id="<?php echo $field['name']; ?>" class="form-control" type="<?php echo $field['type']; ?>" name="<?php echo $field['name']; ?>" placeholder="<?php echo $field['placeholder']; ?>"
-                               value="<?php if($field['name'] !== 'password_check'){ echo(isset($_POST[$field['name']]) ? $_POST[$field['name']] : null); } ?>" <?php echo (isset($field['disabled']) && $field['disabled']) ? 'disabled' : ''; ?> >
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                <?php break;
-                case('select'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>"><?php echo $field['label'] ?></label>
-                        <select class="form-control custom-select" name="<?php echo $field['name']; ?>" id="<?php echo $field['name']; ?>" <?php echo (isset($field['disabled']) && $field['disabled']) ? 'disabled' : ''; ?>>
-                            <?php foreach( $field['options'] as $option => $value ): ?>
-                                <option <?php if(isset($_POST[$field['name']]) && $_POST[$field['name']] == $option){ echo 'selected'; }; ?> value="<?php echo $option; ?>"><?php echo $value; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                <?php break;
-                case('textarea'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label for="<?php echo $field['name']; ?>"><?php echo $field['label'] ?></label>
-                        <textarea class="form-control" name="<?php echo $field['name']; ?>" id="<?php echo $field['name']; ?>" cols="<?php echo $field['cols']; ?>" rows="<?php echo $field['rows']; ?>" <?php echo (isset($field['disabled']) && $field['disabled']) ? 'disabled' : ''; ?>
-                                  placeholder="<?php echo $field['placeholder']; ?>"><?php echo(isset($_POST[$field['name']]) ? $_POST[$field['name']] : null); ?></textarea>
-                        <?php if(array_key_exists('description', $field)): ?>
-                            <span class="description"><?php echo $field['description'] ?></span>
-                        <?php endif; ?>
-                    </div>
-                <?php break;
-                case('checkbox'): ?>
-                    <div class="form-group <?php echo $field['col_size']; ?>">
-                        <label class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" name="<?php echo $field['name']; ?>" id="<?php echo $field['name']; ?>">
-                            <span class="custom-control-indicator"></span>
-                            <span class="custom-control-description"><?php echo $field['label'] ?></span>
-                        </label>
-                    </div>
-                <?php break;
-                case('blank'): ?>
-                    <div class="<?php echo $field['col_size']; ?>"></div>
-                <?php break;
-                case('hidden'): ?>
-                    <input id="<?php echo $field['name']; ?>" type="hidden" name="<?php echo $field['name']; ?>" value="<?php echo(isset($_POST[$field['name']]) ? $_POST[$field['name']] : null); ?>">
-                <?php break;
-            }
-        }
-
-        echo $fields_html;
     }
 
     /**
