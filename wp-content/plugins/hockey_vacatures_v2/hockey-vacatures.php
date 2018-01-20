560329e9558f07f2a1008a201fe7264b6fe2af21
Tim <timvdslik@gmail.com>
2018-01-20 14:04:20 +0100

    begin plugin v2

---
+++ wp-content/plugins/hockey_vacatures_v2/hockey-vacatures.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Hockey Vacatures
+ * Plugin URI:        http://timvanderslik.nl/hockey-vacatures
+ * Description:       Vacatures functionaliteit voor hockey-vacatures.nl
+ * Version:           1.0.0
+ * Author:            Tim van der Slik
+ * Author URI:        http://timvanderslik.nl
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Text Domain:       hockey_vacatures
+ * Domain Path:       /i18n/languages
+ *
+ * @package           Hockey_vacatures
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
---
+++ wp-content/plugins/hockey_vacatures_v2/includes/admin/class-hv-admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 1/20/2018
+ * Time: 1:48 PM
+ */
